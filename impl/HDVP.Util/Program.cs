9afc64d2fd9a61a733ab6bb273913b9e0df0c5f7
Sebastian Krysmanski <skrysmanski@users.noreply.github.com>
2020-11-28 09:19:45 +0100

    Implement HDVP utility (#2)

---
+++ impl/HDVP.Util/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using AppMotor.CliApp.CommandLine;
+
+using HDVP.Util.Properties;
+
+namespace HDVP.Util
+{
+    internal sealed class Program : CliApplicationWithCommands
+    {
+        /// <inheritdoc />
+        protected override string AppDescription => LocalizableResources.AppDescription;
+
+        /// <inheritdoc />
+        protected override IEnumerable<CliVerb> GetVerbs()
+        {
+            yield return new BenchmarkCommand();
+        }
+
+        private static int Main(string[] args)
+        {
+            return Run<Program>(args);
+        }
+    }
+}
